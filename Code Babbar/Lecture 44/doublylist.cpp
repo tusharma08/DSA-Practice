cf772c190c5936cd469995037f295d738593b285
Tushar Sharma <tushar.sh08@gmail.com>
2024-03-07 13:10:43 +0530

    modified:   Code Babbar/Lecture 44/doublylist.cpp 	new file:   Code Babbar/Lecture 44/doublylist.exe

--- Code Babbar/Lecture 44/doublylist.cpp
+++ Code Babbar/Lecture 44/doublylist.cpp
@@ -16,9 +16,23 @@ public:
     }
 };
 
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
 
 int main()
 {
-    
+    Node *node1 = new Node(10);
+    Node *head = node1;
+    Node *tail = node1;
+    print(head);
+
     return 0;
 }
