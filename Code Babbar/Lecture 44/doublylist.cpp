5be0162428159f1039766781abbf5a7d30ae5679
Tushar Sharma <tushar.sh08@gmail.com>
2024-03-07 14:08:12 +0530

    modified:   Code Babbar/Lecture 44/doublylist.cpp 	modified:   Code Babbar/Lecture 44/doublylist.exe

--- Code Babbar/Lecture 44/doublylist.cpp
+++ Code Babbar/Lecture 44/doublylist.cpp
@@ -16,10 +16,45 @@ public:
     }
 };
 
+void insertPostion(Node *&head, Node *&tail, int pos, int data)
+{
+    if (pos == 1)
+    {
+        Node *temp = new Node(data);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+        return;
+    }
+    else
+    {
+        Node *temp = head;
+        int count = 1;
+        while (count < pos - 1)
+        {
+            temp = temp->next;
+            count++;
+        }
+        if (temp->next == NULL)
+        {
+            Node *temp = new Node(data);
+            tail->next = temp;
+            temp->prev = tail;
+            tail = temp;
+            return;
+        }
+        Node *insertNode = new Node(data);
+        insertNode ->next = temp ->next;
+        temp ->next = insertNode;
+        insertNode->prev =temp;
+        temp->next->prev = insertNode;
+    }
+}
+
 void print(Node *&head)
 {
     Node *temp = head;
-    while (temp->next != NULL)
+    while (temp != NULL)
     {
         cout << temp->data << " ";
         temp = temp->next;
@@ -27,12 +62,30 @@ void print(Node *&head)
     cout << endl;
 }
 
+int getLength(Node *&head)
+{
+    Node *temp = head;
+    int len = 0;
+    while (temp != NULL)
+    {
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+
 int main()
 {
     Node *node1 = new Node(10);
     Node *head = node1;
     Node *tail = node1;
+    insertPostion(head,tail,1,12);
+    insertPostion(head,tail,3,14);
+    insertPostion(head,tail,4,21);
     print(head);
+    cout << getLength(head) << endl;
+    cout<<head->data<<endl;
+    cout<<tail->data<<endl;
 
     return 0;
 }
