3cb504c764d6b2702884e47e630b9ecefe81e10e
Tushar Sharma <tushar.sh08@gmail.com>
2024-03-07 14:34:13 +0530

    modified:   Code Babbar/Lecture 44/doublylist.cpp 	modified:   Code Babbar/Lecture 44/doublylist.exe

--- Code Babbar/Lecture 44/doublylist.cpp
+++ Code Babbar/Lecture 44/doublylist.cpp
@@ -14,6 +14,16 @@ public:
         this->prev = NULL;
         this->next = NULL;
     }
+    ~Node()
+    {
+        int value = this->data;
+        if (next != NULL)
+        {
+            next = NULL; 
+            delete next;
+        }
+        cout << "Delete the data: " << value << endl;
+    }
 };
 
 void insertPostion(Node *&head, Node *&tail, int pos, int data)
@@ -44,10 +54,45 @@ void insertPostion(Node *&head, Node *&tail, int pos, int data)
             return;
         }
         Node *insertNode = new Node(data);
-        insertNode ->next = temp ->next;
-        temp ->next = insertNode;
-        insertNode->prev =temp;
+        insertNode->next = temp->next;
+        temp->next = insertNode;
+        insertNode->prev = temp;
         temp->next->prev = insertNode;
+    }
+}
+
+void deleteNode(Node *&head, Node *&tail, int pos)
+{
+    // deleting first Node
+    if (pos == 1)
+    {
+        Node *temp = head;
+        temp->next->prev = NULL;
+        head = head->next;
+        // memory free if Node
+        temp->next = NULL;
+        delete temp;
+    }
+    else
+    {
+        Node *pre = NULL;
+        Node *curr = head;
+        int count = 1;
+        while (count < pos)
+        {
+            pre = curr;
+            curr = curr->next;
+            count++;
+        }
+        // update tail if delete last Node
+        if (curr->next == NULL)
+        {
+            tail = pre;
+            tail->prev = NULL;
+        }
+        pre->next = curr->next;
+        curr->next = NULL;
+        delete curr;
     }
 }
 
@@ -79,13 +124,17 @@ int main()
     Node *node1 = new Node(10);
     Node *head = node1;
     Node *tail = node1;
-    insertPostion(head,tail,1,12);
-    insertPostion(head,tail,3,14);
-    insertPostion(head,tail,4,21);
+    insertPostion(head, tail, 1, 12);
+    insertPostion(head, tail, 3, 14);
+    insertPostion(head, tail, 4, 21);
+    
+    print(head);
+    deleteNode(head,tail,1);
+    deleteNode(head,tail,3);
     print(head);
     cout << getLength(head) << endl;
-    cout<<head->data<<endl;
-    cout<<tail->data<<endl;
+    cout << head->data << endl;
+    cout << tail->data << endl;
 
     return 0;
 }
