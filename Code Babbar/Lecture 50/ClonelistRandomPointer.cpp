016e9b0cbb78fe2607837172badf0826ab6050d2
Tushar Sharma <tushar.sh08@gmail.com>
2024-03-13 22:14:11 +0530

    modified:   Code Babbar/Lecture 50/ClonelistRandomPointer.cpp

--- Code Babbar/Lecture 50/ClonelistRandomPointer.cpp
+++ Code Babbar/Lecture 50/ClonelistRandomPointer.cpp
@@ -1,6 +1,10 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-    return 0;
-}
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//         int sum = n * (n + 1) / 2;
+//         double a = sqrt(sum);
+//         int check = a;
+//         if(a-check == 0) return a;
+//         else return -1;
+//     }
+// };
