54bde4c24cec0c79761d43e321cb40441a5d7424
Tushar Sharma <tushar.sh08@gmail.com>
2024-05-23 22:59:32 +0530

    new file:   Code Babbar/Lecture 54/STL_Stack.cpp 	new file:   Code Babbar/Lecture 54/STL_Stack.exe 	modified:   Code Babbar/Lecture 54/Stack.cpp 	modified:   Code Babbar/Lecture 54/Stack.exe

--- Code Babbar/Lecture 54/Stack.cpp
+++ Code Babbar/Lecture 54/Stack.cpp
@@ -1,18 +1,69 @@
-#include<iostream> 
-#include<stack>
+#include<iostream>
 using namespace std;
 
+class Stack{
+    public:
+        int *arr;
+        int top;
+        int size;
+
+        Stack(int size) {
+            this->size = size;
+            arr = new int[size];
+            top = -1;
+        }
+        
+        void push(int element) {
+            if(size - top > 1) {
+                top++;
+                arr[top] = element;
+            }
+            else {
+                cout<<"Stack Overflow"<<endl;
+            }
+        }
+
+        void pop () {
+            if(top >= 0) {
+                cout<<arr[top]<<endl;
+                top--;
+            }
+            else {
+                cout<<"Stack Empty"<<endl;
+            }
+        }
+
+        int peek () {
+            if(top >= 0) {
+                return arr[top];
+            }
+            else {
+                cout<<"Stack Empty"<<endl;
+                return -1;
+            }
+        }
+
+        bool empty () {
+            if(top >= 0) {
+                return 0;
+            }
+            else {
+                return 1;
+            }
+        }
+};
+
 int main() {
-    stack<int> s;
-
+    Stack s(5);
+    
     cout<<s.empty()<<endl;
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    cout<<s.top()<<endl;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout<<s.peek()<<endl;
+    cout<<s.empty()<<endl;
     s.pop();
-    cout<<s.top()<<endl;
-    cout<<s.empty()<<endl;
-
+    cout<<s.peek()<<endl;
+    
     return 0;
 }
