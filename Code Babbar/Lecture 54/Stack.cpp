a4f9c5a4ac933770daf7ba9c5e440a9636610d1c
Tushar Sharma <tushar.sh08@gmail.com>
2024-05-23 22:32:45 +0530

    modified:   Code Babbar/Lecture 54/Stack.cpp 	new file:   Code Babbar/Lecture 54/Stack.exe

---
+++ Code Babbar/Lecture 54/Stack.cpp
@@ -0,0 +1,18 @@
+#include<iostream> 
+#include<stack>
+using namespace std;
+
+int main() {
+    stack<int> s;
+
+    cout<<s.empty()<<endl;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout<<s.top()<<endl;
+    s.pop();
+    cout<<s.top()<<endl;
+    cout<<s.empty()<<endl;
+
+    return 0;
+}
