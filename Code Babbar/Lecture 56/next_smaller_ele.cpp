79ebeee5994f7ba471f572796372e5f751e042df
Tushar Sharma <tushar.sh08@gmail.com>
2024-05-24 17:39:22 +0530

    new file:   Code Babbar/Lecture 56/next_smaller_ele.cpp

---
+++ Code Babbar/Lecture 56/next_smaller_ele.cpp
@@ -0,0 +1,28 @@
+// Problem statement
+// You are given an array 'ARR' of integers of length N. Your task is to find the next smaller element for each of the array elements.
+
+// Next Smaller Element for an array element is the first element to the right of that element which has a value strictly smaller than that element.
+
+// If for any array element the next smaller element does not exist, you should print -1 for that array element.
+
+// For Example:
+
+// If the given array is [ 2, 3, 1], we need to return [1, 1, -1]. Because for  2, 1 is the Next Smaller element. For 3, 1 is the Next Smaller element and for 1, there is no next smaller element hence the answer for this element is -1.
+
+#include <stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    for(int i = n-1;i>=0;i--) {
+        int num = arr[i];
+        while(s.top()>=num) {
+            s.pop();
+        }
+        ans[i] = s.top();
+        s.push(num);
+    }
+    return ans;
+}
