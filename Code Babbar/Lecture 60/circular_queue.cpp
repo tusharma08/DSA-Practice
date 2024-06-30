59bb95fa7cf61dc0b5022f9b99ab543217f063b0
Tushar Sharma <tushar.sh08@gmail.com>
2024-06-30 15:41:47 +0530

    modified:   Code Babbar/Lecture 60/circular_queue.cpp

--- Code Babbar/Lecture 60/circular_queue.cpp
+++ Code Babbar/Lecture 60/circular_queue.cpp
@@ -1,8 +1,6 @@
 // Problem statement
 // You will be given ‘Q’ queries. You need to implement a circular queue according to those queries. Each query will belong to one of these two types:
-
 // 1 ‘X’: Enqueue element ‘X’ into the end of the nth queue. Returns true if the element is enqueued, otherwise false.
-
 // 2: Dequeue the element at the front of the nth queue. Returns -1 if the queue is empty, otherwise, returns the dequeued element.
 // Note:
 // Enqueue means adding an element to the end of the queue, while Dequeue means removing the element from the front of the queue.
